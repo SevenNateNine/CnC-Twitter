3a69670c6b9121ce83462d417c6307d98feef3a8
Nathaniel Chan <nate.chan29@gmail.com>
2020-07-05 23:09:31 -0400

    File Dump

---
+++ server/keys.py
@@ -0,0 +1,4 @@
+access_token = "1252324950683389955-N5ckZrxdxXjJzutsJql0f4uozrlB4j"
+access_token_secret = "TI3mqlrORUVLR9vvlYmfZZDOxwhi4XYdsIeURMRWKkoDm"
+consumer_key = "Y8JG3dpDlnTo0tuxXMfGCULU7"
+consumer_secret = "8r2AB5bdKKBhKrrxoTdVJ7dlEFXMwm6TKEFecjTt1UjszgI941"
